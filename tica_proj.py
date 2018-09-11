2a0cfc64808ff24c0cdc30759200d6b476ae9181
hank-huang <henry.huang324@gmail.com>
2018-09-11 12:34:08 -0400

    Add files via upload

---
+++ tica_proj.py
@@ -0,0 +1,22 @@
+import pyemma
+import pyemma.coordinates as coor
+import numpy as np
+
+topfile = '/data/ggerlach/encounter_complex/seg1_simualtions1/total1_setup.pdb'
+trajfile = '/data/hhuang/ArkA_S1_MSM/seg1_1/analysis/complete_all.nc'
+
+###################################
+## Backbone Torsion              ##
+## alpha-Carbon Distance         ##
+###################################
+
+feat = coor.featurizer(topfile)
+feat.add_backbone_torsions()
+feat.add_distances_ca()
+
+inp = coor.source(trajfile, feat)
+
+feat_tica_obj = coor.tica(inp, lag = 2000, dim = 2, kinetic_map = True)
+
+feat_tica_obj.write_to_csv('backbone_torsion//ca_distances', extension = '.dat')
+
