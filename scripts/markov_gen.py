365c43d9783b7341d7edc830e164cd6213cf8b05
hank-huang <henry.huang324@gmail.com>
2018-09-11 12:24:16 -0400

    Update markov_gen.py

--- scripts/markov_gen.py
+++ scripts/markov_gen.py
@@ -8,29 +8,24 @@ def markov_generator(outname, tica_in):
 
     Y = np.load(str(tica_in))
     
+    # Kmeans Clustering
     cl = coor.cluster_kmeans(data=Y, k=50, stride=1, max_iter=50)
     cl.save(str('./markov_gen_out/' + outname + '.h5'), 'cluster')
     
     print('\nKMEANS SAVED; SAVING DTRAJ\n')
-
+    
+    # Extracting coordinate data
     dtrajs = cl.dtrajs
     cl.save_dtrajs(trajfiles=['./markov_gen_out/' + outname], output_dir='./markov_gen_out/')
     
     print('\nFINISHED SAVING DTRAJ; STARTING HMM RUNS\n')
     
-    M100 = msm.estimate_hidden_markov_model(dtrajs, 10, 100)
-    print('\nHMM1 COMPLETE\n')
-    M125 = msm.estimate_hidden_markov_model(dtrajs, 10, 125)
-    print('\nHMM2 COMPLETE\n')
-    M150 = msm.estimate_hidden_markov_model(dtrajs, 10, 150)
-    print('\nHMM3 COMPLETE\n')
-    M175 = msm.estimate_hidden_markov_model(dtrajs, 10, 175)    
-    print('\nHMM4 COMPLETE\n')
-        
-    M100.save(str('./markov_gen_out/' + outname + '.h5'), 'lag100')
-    M125.save(str('./markov_gen_out/' + outname + '.h5'), 'lag125')
-    M150.save(str('./markov_gen_out/' + outname + '.h5'), 'lag150')
-    M175.save(str('./markov_gen_out/' + outname + '.h5'), 'lag175')
+    lag = [100, 125, 150, 175]
+    
+    for i in lag:
+        m = msms.estimate_hidden_markov_model(dtrajs, 10, i)
+        m.save(str('./markov_gen_out/' + outname + '.h5'), 'lag' + str(i))
+        print('\nHMM' + str(i) + 'COMPLETE\n')
     
     end = time.time()
     
